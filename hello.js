d9a08321ea0a74422545c323a6fcd268b62fc8c0
basit511 <101107213+basit511@users.noreply.github.com>
2025-05-30 14:55:10 +0530

    Delete hello.js

--- hello.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch('https://railway.com/project/cd75dc8c-c711-4974-9469-b9ee019ed2ea?environmentId=6487b3a4-1f3e-4b62-a6bb-433772900712');
-  const text = await response.text();
-  res.status(200).send(text);
-}
