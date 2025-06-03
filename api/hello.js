6ed89da38c39562980c30d9674c3176c510b2e49
basit511 <101107213+basit511@users.noreply.github.com>
2025-06-03 10:49:53 +0530

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://railway.com/project/cd75dc8c-c711-4974-9469-b9ee019ed2ea?environmentId=6487b3a4-1f3e-4b62-a6bb-433772900712');
+  const response = await fetch('https://railway.com/project/ad061b3a-7c80-476e-bead-a657c1ed4789?environmentId=06cd2965-6a62-406b-9ece-5322c76a9a41');
   const text = await response.text();
   res.status(200).send(text);
 }
